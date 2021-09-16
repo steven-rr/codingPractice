3b81ebf3808586e1afbad051e356739ac682991a
steven-rr <steverelativity@gmail.com>
2021-09-15 23:56:42 -0400

    implementing linkedlist

--- python_practice/main.py
+++ python_practice/main.py
@@ -1,7 +1,29 @@
 
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.next = []
+
+    def append(data):
+        x = 1
+
+class linkedList:
+    """ This linked list class implements a linked list in python. """
+    def __init__(self):
+        """ Constructor """
+        self.head = None
+    
+
+    def append(self, data):
+        if self.head is None:
+            self.head = node(data)
+
+def main():
+    print("hello")
+    x = 1 + 1
+    y = 2+2
+    print(x + y)
 
 
-print("hello")
-x = 1 + 1
-y = 2+2
-print(x + y)
+if __name__ == "__main__":
+    main()
