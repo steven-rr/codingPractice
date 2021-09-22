89e4dc5e283ebd78f5dc27ca162a6f6c1b214bb2
steven-rr <steverelativity@gmail.com>
2021-09-22 00:58:34 -0400

    maxheap implementation in progress...

--- python_practice/maxheap.py
+++ python_practice/maxheap.py
@@ -3,8 +3,10 @@ class maxheap:
     '''
     implements max heap: 
     '''
-    def __init__(self):
+    def __init__(self, maxsize):
         self.size = 0
+        self.maxsize = maxsize
+        self.Heap = [0] * (self.maxsize + 1)
 
     def parent(self, pos):
         return pos // 2
@@ -14,8 +16,33 @@ class maxheap:
     
     def rightChild(self, pos):
         return 2*pos + 1
-        
+    
+    def isLeaf(self,pos):
+        if pos>= (self.size//2) and pos <= self.size:
+            return True
+        return False
 
+    def swap(self, pos1,pos2):
+        self.Heap[pos1], self.Heap[pos2] = (self.Heap[pos2],self.Heap[pos1])
+    
+    def maxHeapify(self, pos):
+        '''
+        Function to maxheapify node at position "pos".
+        '''
+        leftChild = self.Heap[self.leftChild(pos)]
+        rightChild = self.Heap[self.rightChild(pos)]
+
+        # if node is a nonleaf and is smaller than its children, it must exchange spots with one of its children.
+        if not self.isLeaf(pos):
+            if(self.Heap[pos] < leftChild or self.Heap[pos] < rightChild):
+                # the node must exchange spots with the biggest child. if left > right, exchange with left. 
+                if(leftChild > rightChild):
+                   self.swap(pos, self.leftChild(pos))
+                   self.maxHeapify(self.leftChild(pos))
+                #else , exchange with right.
+                else:
+                    self.swap(pos, self.rightChild(pos))
+                    self.maxHeapify(self.rightChild(pos))
 def main ():
     print("hi")
 if __name__ == "__main__":
